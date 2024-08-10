58b6d1595ef91bcd7dbbe404e1fb6682c83a4914
Jose <netinho.workspace@gmail.com>
2024-08-10 14:49:24 +0100

    Update whitelist.sol

--- Contracts/whitelist.sol
+++ Contracts/whitelist.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Husteemas_whitelist{
+    uint public maxwhitelist;
+    mapping (address => bool) public availablelist;
+    address user;
+    uint counter;
+
+    constructor(uint _slots)  {
+        maxwhitelist = _slots;
+    }
+
+    function list() public {
+        require(!availablelist [msg.sender], "user already exist");
+        require( counter < maxwhitelist, "Better luck, slots occupied!");
+        availablelist[msg.sender] = true;
+        counter ++;
+    }
+
+}
